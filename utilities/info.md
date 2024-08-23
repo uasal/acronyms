e064180b7f12e76b93183b4517ab25010bd656e6
sfrinaldi <163890342+sfrinaldi@users.noreply.github.com>
2024-08-22 22:46:28 -0700

    Updating Main  (#1)
    Combined acronyms form cdeep_acronyms.tex, acronyms.tex , and acronym_list.md (on spacecoron_design_docs Overleaf repo) files together and added some new ones.
    Rough script for converting latex acronyms to a markdown list that might be easier to read from (made this in starting weeks for just testing).
    Output markdown file example that is generated from running the latex_acronyms-to-markdown.py script.
    Added ERD acronym
    Added more information / markdown results for reference.
    Added acronyms / abbreviations found in spacecoron_design_docs/ICDs/template.md file.
    Update to combined-acronyms.md
    Adding additional acronyms found.
    Combining original documents to one .tex and removing duplicate acronyms.

---
+++ utilities/info.md
@@ -0,0 +1,7 @@
+# UASAL / acronyms
+For storing the latest versions of acronyms for other repos to pull from. Contains original .tex files found in other repos and on Overleaf which are stored in the [archives](/archives) directory. Previous acronym related tex files were combined into one [combined-acronyms.tex](combined-acronyms.tex) file to potential limit the duplication of acronyms being added to multiple latex files leading to issues in compiling in workflows.
+
+Rough script for generating a markdown file [combined-acronyms.md](combined-acronyms.md) from the [combined-acronyms.tex](combined-acronyms.tex) file provided as well but was a test script.
+
+Results of the last latex to markdown conversion run shown below:
+________________________________________________________________
---
+++ utilities/latex_acronyms-to-markdown.py
@@ -0,0 +1,46 @@
+# For converting combined-acronyms.tex file into an easy-to-read markdown file
+# Requires combined-acronym.tex file to have appropriate line prefixes to work correctly.
+# Refer to the comment section within the acronym.tex file for details.
+
+# Created- 17-MAY-24
+# Author- SFR
+# Last Modified- 17-MAY-24
+# Status- Draft
+
+# Package Imports
+import os
+
+
+# For replacing parts of the line with different values to convert to markdown
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
+# Variables
+output_File = open("combined-acronyms.md", "w")
+input_File = open("combined-acronyms.tex", "r")
+replacements = {"%%+": "##", "%+": "#", "%%": "-", "%": "##", "\\newacronym{": "- ", "$\\textit{": "*", "\\newacronym[": "- ", "plural=": "Plural= ", " firstPlural=": "", "$^{": "^", "{\\gls{": "-> Lower-Case: ","\\gls{": "-> Lower-Case= ", "/\\gls{": "-> Lower-Case: ", "$\\approx$": "~", "description=": "Description= ", "$\\&$": "&", "]{": " -> ", "} ":" -> ", "[p": "P", "$": "", "}{": " -> ", "}": "", "\\epsilon": "$\\epsilon$"}
+current_DIR = os.getcwd()
+# Will need to change this depending on where the acronyms will live in repo
+# acronym_DIR = os.path.join(current_DIR, '/resources/acronyms.tex')
+
+# Markdown Writing
+for line in input_File:
+    adjusted_line = replace_all(line, replacements)
+    if line.startswith("%-"):
+        print("skipping line")
+    else:
+        if line.startswith("% "):
+            output_File.write("---------------------------------\n\n" + adjusted_line + "\n")
+            #print("Writing adjusted lines and adding spacer...")
+        else:
+            output_File.write(adjusted_line)
+            #print("Writing adjusted lines...")
+
+# Closing out files
+output_File.close()
+input_File.close()
+
+print("Markdown Draft Created")
